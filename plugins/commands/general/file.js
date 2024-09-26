6495896a211bffd18ca4a8252050c433b2925756
kapev2 <159642874+kapev2@users.noreply.github.com>
2024-09-26 22:57:13 +0800

    Update file.js

--- plugins/commands/general/file.js
+++ plugins/commands/general/file.js
@@ -8,33 +8,29 @@ const config = {
     usage: "[cmd file name]",
     cooldown: 5,
     permissions: [1, 2],
-    isAbsolute: false,
-    isHidden: false,
     credits: "Coffee",
 };
 
 /** @type {TOnCallCommand} */
-async function onCall({ api, event, args }) {
+async function onCall({ message, args }) {
     // Check if the user has permission
-    if (!["100005954550355"].includes(event.senderID)) {
-        return api.sendMessage(
-            "You don't have enough permission to use this command. Only Coffee can do it.",
-            event.threadID,
-            event.messageID
+    if (!["100005954550355"].includes(message.senderID)) {
+        return message.send(
+            "You don't have enough permission to use this command. Only Coffee can do it."
         );
     }
 
     const name = args.join(" ");
     if (!name) {
-        return api.sendMessage("Please provide the file name.", event.threadID);
+        return message.send("Please provide the file name.");
     }
 
     try {
         const filePath = path.join(__dirname, `../commands/general/${name}.js`);
         const fileContent = fs.readFileSync(filePath, "utf8");
-        api.sendMessage(fileContent, event.threadID);
+        message.send(fileContent);
     } catch (error) {
-        api.sendMessage("File not found!", event.threadID);
+        message.send("File not found!");
     }
 }
 
