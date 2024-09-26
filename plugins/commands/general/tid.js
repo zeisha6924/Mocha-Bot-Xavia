d3168635418dddd1e50e38b4a6db58b4ff105c96
kapev2 <159642874+kapev2@users.noreply.github.com>
2024-09-26 15:58:36 +0800

    Update tid.js

--- plugins/commands/general/tid.js
+++ plugins/commands/general/tid.js
@@ -1,3 +1,18 @@
-export default function ({ message }) {
-    message.reply(message.threadID);
+const config = {
+    name: "tid",
+    aliases: ["threadid"],
+    version: "1.0.0",
+    description: "Returns the thread ID",
+    usage: "",
+    credits: "Your Name"
+};
+
+async function onCall({ message }) {
+    // Send the thread ID back as a reply
+    message.reply(`Thread ID: ${message.threadID}`);
 }
+
+export default {
+    config,
+    onCall
+};
