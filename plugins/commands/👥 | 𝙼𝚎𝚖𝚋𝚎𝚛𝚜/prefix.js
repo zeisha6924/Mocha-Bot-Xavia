eb1708e92c5eb56e45f4867f25dbaf1f36e6de8e
kapev2 <159642874+kapev2@users.noreply.github.com>
2024-09-28 00:50:27 +0800

    Update prefix.js

--- plugins/commands/👥 | 𝙼𝚎𝚖𝚋𝚎𝚛𝚜/prefix.js
+++ plugins/commands/👥 | 𝙼𝚎𝚖𝚋𝚎𝚛𝚜/prefix.js
@@ -6,56 +6,30 @@ const config = {
     category: "𝙼𝚎𝚖𝚋𝚎𝚛𝚜",
     cooldown: 5,
     credits: "XaviaTeam"
-}
+};
 
-const langData = {
-    "en_US": {
-        "prefix.get": "Default prefix: {default}\nCurrent prefix: {current}",
-        "prefix.set": "Prefix has been set to {newPrefix}",
-        "prefix.tooLong": "Prefix must be less than 5 characters",
-        "notGroup": "This command only works in group",
-        "threadDataNotExists": "Thread data not exists"
-    },
-    "vi_VN": {
-        "prefix.get": "Prefix mặc định: {default}\nPrefix hiện tại: {current}",
-        "prefix.set": "Prefix đã được đặt thành {newPrefix}",
-        "prefix.tooLong": "Prefix phải ít hơn 5 ký tự",
-        "notGroup": "Lệnh này chỉ hoạt động trong nhóm",
-        "threadDataNotExists": "Dữ liệu nhóm không tồn tại"
-    },
-    "ar_VN": {
-        "prefix.get": "بادئة افتراضية: {default}\nالبادئة الحالية: {current}",
-        "prefix.set": "تم تعيين البادئة على {newPrefix}",
-        "prefix.tooLong": "يجب أن تكون البادئة أقل من 5 أحرف",
-        "notGroup": "هذا الأمر يعمل فقط في المجموعة",
-        "threadDataNotExists": "بيانات المجموعة غير موجودة"
-    }
-}
-
-async function onCall({ message, args, data, getLang, prefix }) {
+async function onCall({ message, args, data, prefix }) {
     const { isGroup, threadID } = message;
     const { thread } = data;
 
-    if (!isGroup) return message.reply(getLang("notGroup"));
-    if (!thread?.info?.threadID) return message.reply(getLang("threadDataNotExists"));
+    if (!isGroup) return message.reply("This command only works in group.");
+    if (!thread?.info?.threadID) return message.reply("Thread data does not exist.");
     if (!thread.data) thread.data = {};
 
-    if (!args[0]) return message.reply(getLang("prefix.get", {
-        default: global.config.PREFIX,
-        current: prefix
-    }));
+    if (!args[0]) {
+        return message.reply(`Default prefix: ${global.config.PREFIX}\nCurrent prefix: ${prefix}`);
+    }
 
     const newPrefix = args[0];
-    if (newPrefix.length > 5) return message.reply(getLang("prefix.tooLong"));
+    if (newPrefix.length > 5) return message.reply("Prefix must be less than 5 characters.");
 
     await global.controllers.Threads.updateData(threadID, { prefix: newPrefix });
     global.api.changeNickname(`[ ${newPrefix} ] ${global.config.NAME || "Xavia"}`, threadID, global.botID, (_) => {
-        message.reply(getLang("prefix.set", { newPrefix }));
+        message.reply(`Prefix has been set to ${newPrefix}.`);
     });
 }
 
 export default {
     config,
-    langData,
     onCall
-}
+};
