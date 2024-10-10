755be93793d0b365606d203f8795ff421e236511
kapev2 <159642874+kapev2@users.noreply.github.com>
2024-10-10 16:15:34 +0800

    Delete plugins/commands/example/example_3.js

--- plugins/commands/example/example_3.js
+++
@@ -1,7 +0,0 @@
-// no config provided so will use default config
-// with name will be the file name
-
-/** @type {TOnCallCommand} */
-export default function ({ message, args, getLang, extra, data, userPermissions, prefix }) {
-    // code here
-}
